001f9d6cb863f70ba863a5bf3c45a5f0b4229fe6
Julia.S <der.lavender@gmail.com>
2022-08-10 16:41:44 +0300

    задача 5

---
+++ task005_modul/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу вычисления модуля числа.
+//2 -> 2
+int Prompt(string message)
+{
+    System.Console.Write(message); 
+    string readValue = Console.ReadLine(); 
+    int result = int.Parse(readValue); 
+    return result; 
+}
+
+int N = Prompt("Введите число ");
+Console.WriteLine(Math.Abs(N));
