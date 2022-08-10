8ab75a1dd6ac369a226b7956698ec0ec5eeb2a70
Julia.S <der.lavender@gmail.com>
2022-08-10 16:29:16 +0300

    задача 4

---
+++ task004/Program.cs
@@ -0,0 +1,19 @@
+﻿//апишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+//2 -> " -2, -1, 0, 1, 2".
+
+int Prompt(string message)
+{
+    System.Console.Write(message); 
+    string readValue = Console.ReadLine(); 
+    int result = int.Parse(readValue); 
+    return result; 
+}
+
+int N = Prompt("Введите число N ");
+
+for (int i = -N; i <=N; i++)
+{
+    Console.Write(i );
+}
