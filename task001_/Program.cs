dd7747884b4e2b0c341ae66ceb0bbc85f8f77ed8
Julia.S <der.lavender@gmail.com>
2022-08-10 15:33:36 +0300

    zada4a 1 i 2

---
+++ task001_/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+int Prompt(string message)
+{
+System.Console.Write(message); // Вывести сообщение
+string readValue = Console.ReadLine(); // Считавает с консоли строку
+int result = int.Parse(readValue); // Преобрадует строку в целое число
+return result; // Возвращает результат
+}
+
+
+int value1 = Prompt("Введите число >"); // Вводим значение
+int value2 = Prompt("Введите квадрат >");
+
+if (value2 == value1 * value1)
+{
+    Console.WriteLine ("Число b является квадратом числа а");
+}
+else
+{
+    Console.WriteLine ("Число b не является квадратом числа а");
+
+}
+
---
+++ task002_day/Program.cs
@@ -0,0 +1,55 @@
+﻿//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+int Prompt(string message)
+{
+    System.Console.Write(message); 
+    string readValue = Console.ReadLine(); 
+    int result = int.Parse(readValue); 
+    return result; 
+}
+
+int daynumber = Prompt("Введите номер дня недели  ");
+
+if (daynumber == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else
+if (daynumber == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else
+if (daynumber == 3)
+{
+    Console.WriteLine("Среда");
+}
+else
+
+if (daynumber == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else
+
+if (daynumber == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else
+
+if (daynumber == 6)
+{
+    Console.WriteLine("Суббота");
+}
+
+else
+
+if (daynumber == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+else Console.WriteLine("Вводите значения от 1 до 7, дней недели только семь!");
+
+
